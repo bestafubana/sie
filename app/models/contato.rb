2ebf54af59c3ec0e46cd4ef2fbfc0bb8a650299c
Natanael Silva <natanaelsilva@gmail.com>
2013-01-10 20:34:17 -0300

    Adicionando validações no model contatos

--- app/models/contato.rb
+++ app/models/contato.rb
@@ -1,3 +1,8 @@
 class Contato < ActiveRecord::Base
   attr_accessible :departamento, :email, :empresa, :id, :nome, :telefone
+  
+  validates :nome, :telefone, :email, :presence => true
+  validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }, :uniqueness => { :case_sensitive => false } 
+  validates :telefone, :numericality => true
+  
 end
