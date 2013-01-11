04964d48184f43356b2905fe6392df67f3e338ae
thiagofranca <undeadthiago@gmail.com>
2013-01-10 21:30:11 -0300

    mensagens de validação

--- app/models/contato.rb
+++ app/models/contato.rb
@@ -1,8 +1,8 @@
 class Contato < ActiveRecord::Base
   attr_accessible :departamento, :email, :empresa, :id, :nome, :telefone
   
-  validates :nome, :telefone, :email, :presence => true
-  validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }, :uniqueness => { :case_sensitive => false } 
-  validates :telefone, :numericality => true
+  validates :nome, :telefone, :email, :presence => { :message => "é um campo obrigatório"}
+  validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i , :message => "está com formato inválido"}, :uniqueness => { :case_sensitive => false } 
+  validates :telefone, :numericality => { :message => "é um campo numérico"}
   
 end
