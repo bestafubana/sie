b508c7701cfca5a9279cdb23f57070c3ee1ec6fd
Natanael Silva <natanaelsilva@gmail.com>
2013-01-20 18:56:59 -0300

    agora o link de contatos marca o botao certo no menu

--- app/models/consultado.rb
+++ app/models/consultado.rb
@@ -1,3 +1,14 @@
 class Consultado < ActiveRecord::Base
-  attr_accessible :caminho_foto, :cpf, :data_nasc, :data_obito, :endereco, :id, :nome, :nome_mae, :observacoes, :sexo, :situacao_cadastral, :titulo_eleitor, :unidade_adm
+  attr_accessible :caminho_foto,
+                  :cpf, :data_nasc,
+                  :data_obito,
+                  :endereco,
+                  :id,
+                  :nome,
+                  :nome_mae,
+                  :observacoes,
+                  :sexo,
+                  :situacao_cadastral,
+                  :titulo_eleitor,
+                  :unidade_adm
 end
